cab5606ccf7838e128e7b50c79b8929a90f05a5e
Ryan SU <yuansu.china.work@gmail.com>
2023-08-04 09:42:51 +0800

    chore: regen ent codes

--- ent/smslog.go
+++ ent/smslog.go
@@ -19,9 +19,9 @@ type SmsLog struct {
 	// ID of the ent.
 	// UUID
 	ID uuid.UUID `json:"id,omitempty"`
-	// CreatedAt holds the value of the "created_at" field.
+	// Create Time | 创建日期
 	CreatedAt time.Time `json:"created_at,omitempty"`
-	// UpdatedAt holds the value of the "updated_at" field.
+	// Update Time | 修改日期
 	UpdatedAt time.Time `json:"updated_at,omitempty"`
 	// The target phone number | 目标电话
 	PhoneNumber string `json:"phone_number,omitempty"`
