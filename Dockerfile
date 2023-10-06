b0329332b1293b78166d2fe9f919406c0b4d27ab
Ryan Su <yuansu.china.work@gmail.com>
2023-10-06 11:28:12 +0800

    chore: update deps

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,3 @@
-FROM golang:1.20.6-alpine3.17 as builder
-
-# Define the project name | 定义项目名称
-ARG PROJECT=mcms
-
-WORKDIR /build
-COPY . .
-
-RUN go env -w GO111MODULE=on \
-    && go env -w CGO_ENABLED=0 \
-    && go env \
-    && go mod tidy \
-    && go build -ldflags="-s -w" -o /build/${PROJECT}_rpc ${PROJECT}.go
-
 FROM alpine:latest
 
 # Define the project name | 定义项目名称
@@ -19,7 +5,7 @@ ARG PROJECT=mcms
 # Define the config file name | 定义配置文件名
 ARG CONFIG_FILE=mcms.yaml
 # Define the author | 定义作者
-ARG AUTHOR="yuansu.china.work@gmail.com"
+ARG AUTHOR="example@example.com"
 
 LABEL org.opencontainers.image.authors=${AUTHOR}
 
@@ -27,9 +13,7 @@ WORKDIR /app
 ENV PROJECT=${PROJECT}
 ENV CONFIG_FILE=${CONFIG_FILE}
 
-COPY --from=builder /build/${PROJECT}_rpc ./
-COPY --from=builder /build/etc/${CONFIG_FILE} ./etc/
-
-EXPOSE 9105
+COPY ./${PROJECT}_rpc ./
+COPY ./etc/${CONFIG_FILE} ./etc/
 
 ENTRYPOINT ./${PROJECT}_rpc -f etc/${CONFIG_FILE}
--- ent/emaillog/where.go
+++ ent/emaillog/where.go
@@ -472,32 +472,15 @@ func ProviderContainsFold(v string) predicate.EmailLog {
 
 // And groups predicates with the AND operator between them.
 func And(predicates ...predicate.EmailLog) predicate.EmailLog {
-	return predicate.EmailLog(func(s *sql.Selector) {
-		s1 := s.Clone().SetP(nil)
-		for _, p := range predicates {
-			p(s1)
-		}
-		s.Where(s1.P())
-	})
+	return predicate.EmailLog(sql.AndPredicates(predicates...))
 }
 
 // Or groups predicates with the OR operator between them.
 func Or(predicates ...predicate.EmailLog) predicate.EmailLog {
-	return predicate.EmailLog(func(s *sql.Selector) {
-		s1 := s.Clone().SetP(nil)
-		for i, p := range predicates {
-			if i > 0 {
-				s1.Or()
-			}
-			p(s1)
-		}
-		s.Where(s1.P())
-	})
+	return predicate.EmailLog(sql.OrPredicates(predicates...))
 }
 
 // Not applies the not operator on the given predicate.
 func Not(p predicate.EmailLog) predicate.EmailLog {
-	return predicate.EmailLog(func(s *sql.Selector) {
-		p(s.Not())
-	})
+	return predicate.EmailLog(sql.NotPredicates(p))
 }
